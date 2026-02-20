9a80651d0a0d459518cb43be2b21a9a5c96e23af
Pedro <4291033+pedroceriotti@users.noreply.github.com>
2026-02-20 18:46:25 +0000

    feat: Create base code to load data from Nekt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
+
+import nekt
+
+nekt.data_access_token = st.secrets["DATA_ACCESS_TOKEN"]
+
+try:
+    cidades_df = nekt.load_table(layer_name="Raw", table_name="cidades").toPandas()
+    st.write(cidades_df)  
+    st.write("Dataframe loaded successfully!")
+except Exception as e:
+    st.error(f"Error loading data: {e}")
+
+########
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
